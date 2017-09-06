aa4ea53affd0aca86962fbb94d5f0a893ebfdbf9
SATOSHI NAKAJIMA <satoshi.nakajima@gmail.com>
2017-09-06 09:45:50 -0700

    Xcode project

---
+++ motion/motion/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  motion
+//
+//  Created by SATOSHI NAKAJIMA on 9/6/17.
+//  Copyright © 2017 SATOSHI NAKAJIMA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
